0628dcc3bb7dada9324415fce49b5afa7ec67262
Sergey <duxxless53@ya.ru>
2022-08-06 17:34:01 +0300

    Initial Commit

---
+++ Listed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Listed
+//
+//  Created by Duxxless on 06.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
